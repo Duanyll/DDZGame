123aa267f1e19f91f5566d68884b2129b437cd12
duanyll <xiaoguo2004@sina.cn>
2018-07-21 15:16:00 +0800

    添加License，添加Card结构

---
+++ GameServer/GameServer/GameruleHandler.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer
+{
+    class GameruleHandler
+    {
+        private struct Card
+        {
+            public enum Color
+            {
+                Hongtao,
+                Fangpian,
+                Heitao,
+                Meihua,
+                Joker
+            };
+            public Color color;
+            public enum Value
+            {
+                Three,
+                Four,
+                Five,
+                Six,
+                Seven,
+                Eight,
+                Nine,
+                Ten,
+                J,
+                Q,
+                K,
+                A,
+                Two,
+                Grey,
+                Red
+            }
+            public Value value;
+            public override string ToString()
+            {
+                string ret = "";
+                char col = (char)('A' + (int)color);
+                ret += col;
+                char val = (char)('a' + (int)value);
+                ret += val;
+                return ret;
+            }
+            public Card(char col,char val)
+            {
+                color = (Color)(col - 'A');
+                value = (Value)(val - 'a');
+            }
+            public static bool operator ==(Card A,Card B)
+            {
+                return A.value == B.value;
+            }
+            public static bool operator !=(Card A,Card B)
+            {
+                return !(A == B);
+            }
+            public static bool operator <(Card A,Card B)
+            {
+                return A.value < B.value;
+            }
+            public static bool operator >(Card A,Card B)
+            {
+                return A.value > B.value;
+            }
+            public static bool operator >=(Card A,Card B)
+            {
+                return !(A < B);
+            }
+            public static bool operator <=(Card A,Card B)
+            {
+                return !(A > B);
+            }
+        }
+
+        class Selection : List<Card>
+        {
+            public enum Type
+            {
+                None,
+                Single,
+                Double,
+                ThreeWithOne,
+                Chair,
+                Line,
+                Plane,
+                Bomb
+            };
+            Type type;
+        }
+
+        public GameruleHandler()
+        {
+            Program.Log("已调用GameruleHandler构造函数");
+        }
+
+        void Work()
+        {
+
+        }
+    }
+}
--- GameServer/GameServer/Program.cs
+++ GameServer/GameServer/Program.cs
@@ -8,8 +8,26 @@ namespace GameServer
 {
     class Program
     {
+        public static void Log(string a)
+        {
+            Console.WriteLine(a);
+        }
+
+        static GameruleHandler handler;
+
         static void Main(string[] args)
         {
+            Console.WriteLine("DDZ Game Server Copyright (C) 2018  Duanyll");
+            Console.WriteLine("This program comes with ABSOLUTELY NO WARRANTY; ");
+            Console.WriteLine("This is free software, and you are welcome to redistribute it");
+            Console.WriteLine("under certain conditions;");
+
+            Log("服务器程序已启动");
+            handler = new GameruleHandler();
+
+            Log("即将终止服务器程序,请按任意键退出");
+            Console.ReadKey();
+            return;
         }
     }
 }
