291363ef96da7f57c4bc5b6929b9804dfcc34df8
duanyll <xiaoguo2004@sina.cn>
2018-07-21 16:18:30 +0800

    编写了程序主逻辑（GameruleHandler.Work()）

--- GameServer/GameServer/GameruleHandler.cs
+++ GameServer/GameServer/GameruleHandler.cs
@@ -91,17 +91,154 @@ namespace GameServer
                 Plane,
                 Bomb
             };
-            Type type;
+            public Type type;
+
+            public Selection()
+            {
+                type = Type.None;
+            }
+
+            public Selection(string a)
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool CanMatch(Selection Last)
+            {
+                throw new NotImplementedException();
+            }
         }
+
+        List<Card>[] cards;
+        List<Card> baseCard;
+        Random rand = new Random();
 
         public GameruleHandler()
         {
             Program.Log("已调用GameruleHandler构造函数");
         }
 
-        void Work()
+        public void Work()
         {
+            Program.Log("已开局");
+            cards = new List<Card>[3];
+            CreateCards();
+            SendCardList(0);
+            SendCardList(1);
+            SendCardList(2);
+            int now = rand.Next(2);
+            bool LandlordSelected = false;
+            for(int i = 1; i <= 3; i++)
+            {
+                if (AskLandlord(now))
+                {
+                    LandlordSelected = true;
+                    break;
+                }
+                else
+                {
+                    now++;
+                    now %= 3;
+                }
+            }
+            if (LandlordSelected)
+            {
+                Program.Log(String.Format("{0}号玩家是地主", now));
+                cards[now].AddRange(baseCard);
+                SendBaseCard();
+                Selection Last = new Selection();
+                int Owner = now;
+                while (true)
+                {
+                    AnnounceRound(now);
+                    SendCardList(now);
+                    Selection selection = GetSelection(now);
+                    AnnounceSelection(now, selection);
+                    while ((!((now != Owner) && selection.type == Selection.Type.None)) && !selection.CanMatch(Last))
+                    {
+                        TellSelectionFail(now);
+                        selection = GetSelection(now);
+                    }
+                    if (selection.type != Selection.Type.None)
+                    {
+                        Last = selection;
+                    }
+                    AnnounceSelection(now, selection);
+                    for(int i = 0; i < cards[now].Count; i++)
+                    {
+                        if (selection.Contains(cards[now][i]))
+                        {
+                            cards[i].RemoveAt(i);
+                            i--;
+                        }
+                    }
+                    if (cards[now].Count == 0)
+                    {
+                        AnnounceWinner(now);
+                        break;
+                    }
+                    else
+                    {
+                        SendCardList(now);
+                        now++;
+                        now %= 3;
+                    }
+                }
+            }
+            else
+            {
+                CalculateScore();
+            }
+        }
 
+        private void AnnounceWinner(int now)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AnnounceSelection(int now, Selection selection)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void TellSelectionFail(int now)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Selection GetSelection(int now)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AnnounceRound(int now)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SendBaseCard()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CalculateScore()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool AskLandlord(int now)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SendCardList(int v)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CreateCards()
+        {
+            throw new NotImplementedException();
         }
     }
 }
